8e4b8c02f323652a08ca6cb3e1a1d936d331e95e
muhammetozdemir10 <127602475+muhammetozdemir10@users.noreply.github.com>
2023-05-16 21:04:21 +0300

    Update Program.cs
    Rerun the program again based on user response
    Explained the Try and Catch

--- Program.cs
+++ Program.cs
@@ -31,47 +31,80 @@
             return a / b;
         }
 
-        static void Main(string[] args)
+        static void Greeting()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Hesap makineme hos geldiniz.");
             Console.ResetColor();
 
+        }
+
+
+        static void App()
+        {
             Console.WriteLine("Lutfen yapmak istediginiz islemi seciniz");
             Console.WriteLine("Secebileginiz islemler: (+, -, *, /)");
             string? sign = Console.ReadLine();
 
-            Console.WriteLine("Birinci sayiyi giriniz");
-            int number1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ikinci sayiyi giriniz");
-            int number2 = int.Parse(Console.ReadLine());
-
-            switch (sign)
+            try
             {
-                case "+":
-                    Console.WriteLine(Sum(number1, number2));
-                    break;
+                Console.WriteLine("Birinci sayiyi giriniz");
+                int number1 = int.Parse(Console.ReadLine());
 
-                case "-":
-                    Console.WriteLine(Minus(number1, number2));
-                    break;
+                Console.WriteLine("Ikinci sayiyi giriniz");
+                int number2 = int.Parse(Console.ReadLine());
 
-                case "*":
-                    Console.WriteLine(Multiply(number1, number2));
-                    break;
+                switch (sign)
+                {
+                    case "+":
+                        Console.WriteLine(Sum(number1, number2));
+                        break;
 
-                case "/":
-                    Console.WriteLine(Div(number1, number2));
-                    break;
+                    case "-":
+                        Console.WriteLine(Minus(number1, number2));
+                        break;
 
-                default: 
-                    Console.WriteLine("Gecersiz bir islem girdiniz");
-                    break;
+                    case "*":
+                        Console.WriteLine(Multiply(number1, number2));
+                        break;
+
+                    case "/":
+                        Console.WriteLine(Div(number1, number2));
+                        break;
+
+                    default:
+                        Console.WriteLine("Gecersiz bir islem girdiniz");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.ForegroundColor = ConsoleColor.DarkRed;
+                //Console.WriteLine(ex);
+                //Console.ResetColor();
+                Console.WriteLine("Bir hata olustu lutfen tekrardan deneyiniz.");
+                App();
             }
 
-            // TODO: Kullaniciya baska bir islem yapmak istiyorsa kodu birdaha calistirmayi dene
+            
+
+            Console.WriteLine("Yapmak isteginiz baska bir islem varmi? Y/N");
+            string? Cevap = Console.ReadLine();
+            if (Cevap.ToLower() == "y")
+            {
+                App();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Greeting();
+            App();
+
+            // COMPLETE: Kullaniciya baska bir islem yapmak istiyorsa kodu birdaha calistir
             // TODO: Kullanici sayi yerine harf girerse hatali bir mesaj versin ve bir kez daha girmesini istesin yada bastan islemi alsin.
+
 
         }
 
